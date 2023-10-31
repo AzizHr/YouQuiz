0afc3ba073be7e7f3f902a19f44373296333c425
azizHr <azizharkati20@gmail.com>
2023-10-31 09:25:11 +0100

    configuring some files

--- api/src/main/java/com/quiz/api/controllers/StudentController.java
+++ api/src/main/java/com/quiz/api/controllers/StudentController.java
@@ -18,27 +18,19 @@ public class StudentController {
     public StudentController(StudentService service) {
         studentService = service;
     }
-
     @GetMapping
     public List<Student> students() {
         return studentService.getStudents();
     }
-
     @PostMapping()
     public Student save(@RequestBody Student student) {
         return studentService.save(student);
     }
-
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Integer id) {
         studentService.delete(id);
         return ResponseEntity.ok("Student with ID " + id + " has been deleted.");
     }
 
-//    @PutMapping ("/{id}")
-//    public ResponseEntity<String> update(@PathVariable Integer id) {
-//        studentService.delete(id);
-//        return ResponseEntity.ok("Student with ID " + id + " has been deleted.");
-//    }
 
 }
--- api/src/main/java/com/quiz/api/repositories/StudentRepository.java
+++ api/src/main/java/com/quiz/api/repositories/StudentRepository.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-
-
-}
+public interface StudentRepository extends JpaRepository<Student, Integer> {}
